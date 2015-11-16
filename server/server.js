6dd92aad9a6e7200355a3980b5453a952a007723
yangli1990 <yangli1990@live.com.au>
2015-11-16 17:50:03 +1100

    add login

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 var request = require('request');
-
+var bodyParser = require('body-parser');
+var _ = require('lodash');
 var PORT = process.env.PORT || 3001 ;
 
 var yahoo = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{stock}.AX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+var logins = {};
+
+app.use( bodyParser.json() ); 
 
 app.get('/finance/:stock', function(req, res){
 	request(yahoo.replace('{stock}', req.params.stock), function(error, response, body){
@@ -22,6 +27,25 @@ app.get('/finance/:stock', function(req, res){
 	  	}
 		
 	});
+});
+
+app.post('/register', function(req, res){
+	var username = req.body.username;
+	if (logins[username]) {
+		return res.send('user already exists');
+	} else {
+		logins[username] = req.body;
+	}
+});
+
+app.post('/login', function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	if (login[username].password === password) {
+		res.send(_.omit(login[username], 'password'));
+	} else {
+		res.send('incorrect login');
+	}
 });
 
 app.listen(PORT, function() {
